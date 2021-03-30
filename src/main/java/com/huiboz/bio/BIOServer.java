58d74f5ac1a599c0311ccf231bf4f0da4a98c8fa
Huibo Zhao <zhaohuibo95@gmail.com>
2021-03-29 23:42:34 -0700

    complete bio example

--- src/main/java/com/huiboz/bio/BIOServer.java
+++ src/main/java/com/huiboz/bio/BIOServer.java
@@ -8,6 +8,7 @@ import java.util.concurrent.Executors;
 
 /*
 A blocking IO server
+telnet localhost 7777
  */
 public class BIOServer {
     final static int PORT_NUM = 7777;
@@ -40,9 +41,11 @@ public class BIOServer {
             executorService.execute(new Thread() {
                 public void run() {
                     try {
+                        System.out.println(Thread.currentThread().getName());
                         final byte[] bytes = new byte[1024];
                         final InputStream inputStream = clientSocket.getInputStream();
                         while (true) {
+                            System.out.println("check: " + Thread.currentThread().getName());
                             final int read = inputStream.read(bytes);
                             if (read != -1) {
                                 System.out.println("read: " + new String(bytes, 0 , read));
